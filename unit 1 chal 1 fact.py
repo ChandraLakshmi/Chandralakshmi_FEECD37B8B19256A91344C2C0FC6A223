1b0c865eaa540e769919cb3ba3c178e44eebbf90
ChandraLakshmi <144940850+ChandraLakshmi@users.noreply.github.com>
2023-09-15 15:08:17 +0530

    Add files via upload

---
+++ unit 1 chal 1 fact.py
@@ -0,0 +1,16 @@
+def factorial(n):
+    if(n == 0):
+       return 1
+    i = n
+    fact = 1
+      
+    while(n / i != n):
+        fact = fact * i
+        i -= 1
+          
+    return fact
+num = 5;
+print("Factorial of", num, "is",
+factorial(num))
+
+   
